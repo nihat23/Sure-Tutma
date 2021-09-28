ac3931a2f75fa3f73721008a4e66cb8ed9da4c06
nihat23 <62906442+nihat23@users.noreply.github.com>
2021-09-28 23:32:03 +0300

    sure tutma
    Süre tutma kayıt program

---
+++ Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿
+namespace süre_tutma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1Baslat = new System.Windows.Forms.Button();
+            this.button2DurDur = new System.Windows.Forms.Button();
+            this.button3Sifirla = new System.Windows.Forms.Button();
+            this.button4Kaydet = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.button1Cikar = new System.Windows.Forms.Button();
+            this.button1Arama = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1Yenile = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(99, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(235, 31);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1Baslat
+            // 
+            this.button1Baslat.BackColor = System.Drawing.Color.Maroon;
+            this.button1Baslat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1Baslat.ForeColor = System.Drawing.SystemColors.Menu;
+            this.button1Baslat.Location = new System.Drawing.Point(100, 113);
+            this.button1Baslat.Name = "button1Baslat";
+            this.button1Baslat.Size = new System.Drawing.Size(73, 32);
+            this.button1Baslat.TabIndex = 2;
+            this.button1Baslat.Text = "Başlat";
+            this.button1Baslat.UseVisualStyleBackColor = false;
+            this.button1Baslat.Click += new System.EventHandler(this.button1Baslat_Click);
+            // 
+            // button2DurDur
+            // 
+            this.button2DurDur.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button2DurDur.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2DurDur.Location = new System.Drawing.Point(181, 113);
+            this.button2DurDur.Name = "button2DurDur";
+            this.button2DurDur.Size = new System.Drawing.Size(73, 32);
+            this.button2DurDur.TabIndex = 3;
+            this.button2DurDur.Text = "Durdur";
+            this.button2DurDur.UseVisualStyleBackColor = false;
+            this.button2DurDur.Click += new System.EventHandler(this.button2DurDur_Click);
+            // 
+            // button3Sifirla
+            // 
+            this.button3Sifirla.BackColor = System.Drawing.Color.LawnGreen;
+            this.button3Sifirla.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3Sifirla.Location = new System.Drawing.Point(262, 113);
+            this.button3Sifirla.Name = "button3Sifirla";
+            this.button3Sifirla.Size = new System.Drawing.Size(73, 32);
+            this.button3Sifirla.TabIndex = 4;
+            this.button3Sifirla.Text = "Sıfırla";
+            this.button3Sifirla.UseVisualStyleBackColor = false;
+            this.button3Sifirla.Click += new System.EventHandler(this.button3Sifirla_Click);
+            // 
+            // button4Kaydet
+            // 
+            this.button4Kaydet.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.button4Kaydet.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4Kaydet.Location = new System.Drawing.Point(100, 151);
+            this.button4Kaydet.Name = "button4Kaydet";
+            this.button4Kaydet.Size = new System.Drawing.Size(234, 32);
+            this.button4Kaydet.TabIndex = 5;
+            this.button4Kaydet.Text = "Kaydet ";
+            this.button4Kaydet.UseVisualStyleBackColor = false;
+            this.button4Kaydet.Click += new System.EventHandler(this.button4Kaydet_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(100, 64);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(235, 31);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(5, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Adı Soyad :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(45, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Süre :";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.linkLabel1.Location = new System.Drawing.Point(781, 290);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(55, 13);
+            this.linkLabel1.TabIndex = 10;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Nihat Beyi";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // button1Cikar
+            // 
+            this.button1Cikar.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.button1Cikar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1Cikar.Location = new System.Drawing.Point(99, 189);
+            this.button1Cikar.Name = "button1Cikar";
+            this.button1Cikar.Size = new System.Drawing.Size(234, 32);
+            this.button1Cikar.TabIndex = 11;
+            this.button1Cikar.Text = "Çıkar";
+            this.button1Cikar.UseVisualStyleBackColor = false;
+            this.button1Cikar.Click += new System.EventHandler(this.button1Cikar_Click);
+            // 
+            // button1Arama
+            // 
+            this.button1Arama.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.button1Arama.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1Arama.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.button1Arama.Location = new System.Drawing.Point(99, 227);
+            this.button1Arama.Name = "button1Arama";
+            this.button1Arama.Size = new System.Drawing.Size(234, 32);
+            this.button1Arama.TabIndex = 13;
+            this.button1Arama.Text = "Arama Yap";
+            this.button1Arama.UseVisualStyleBackColor = false;
+            this.button1Arama.Click += new System.EventHandler(this.button1Arama_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Location = new System.Drawing.Point(340, 16);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(496, 271);
+            this.dataGridView1.TabIndex = 17;
+            // 
+            // button1Yenile
+            // 
+            this.button1Yenile.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.button1Yenile.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1Yenile.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.button1Yenile.Location = new System.Drawing.Point(99, 265);
+            this.button1Yenile.Name = "button1Yenile";
+            this.button1Yenile.Size = new System.Drawing.Size(234, 32);
+            this.button1Yenile.TabIndex = 18;
+            this.button1Yenile.Text = "Yenile";
+            this.button1Yenile.UseVisualStyleBackColor = false;
+            this.button1Yenile.Click += new System.EventHandler(this.button1Yenile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Lime;
+            this.ClientSize = new System.Drawing.Size(848, 312);
+            this.Controls.Add(this.button1Yenile);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1Arama);
+            this.Controls.Add(this.button1Cikar);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4Kaydet);
+            this.Controls.Add(this.button3Sifirla);
+            this.Controls.Add(this.button2DurDur);
+            this.Controls.Add(this.button1Baslat);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Süre tutma";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1Baslat;
+        private System.Windows.Forms.Button button2DurDur;
+        private System.Windows.Forms.Button button3Sifirla;
+        private System.Windows.Forms.Button button4Kaydet;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button button1Cikar;
+        private System.Windows.Forms.Button button1Arama;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1Yenile;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.OleDb;//acces kutuphanesini eklıyoruz..
+
+namespace süre_tutma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OleDbConnection baglantim = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Surem.accdb");
+        OleDbDataReader oku;
+        OleDbCommand komutver;
+        DataTable tablo;
+        OleDbDataAdapter liste;
+       
+
+        void bilgilerimi_goster()
+        {
+            baglantim.Open();
+            liste = new OleDbDataAdapter("select id as[Sıra],AdSoyad as [Adı Soyadı],Sure as [Süresi] from Bilgiler", baglantim);
+            tablo = new DataTable();
+            liste.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            baglantim.Close();
+        }
+        bool kontrol;
+        void mukerrer()
+        {
+            baglantim.Open();
+            komutver = new OleDbCommand("select * from Bilgiler where AdSoyad='" + textBox1.Text + "'", baglantim);
+            oku = komutver.ExecuteReader();
+            if (oku.Read())
+            {
+                kontrol = false;
+            }
+            else
+            {
+                kontrol = true;
+            }
+            baglantim.Close();
+
+        }
+
+        int saat = 0, dakika = 0, saniye = 0, salise = 0;
+        private void button3Sifirla_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            salise = 0;
+            saniye = 0;
+            dakika = 0;
+            saat = 0;
+
+            textBox2.Text = saat.ToString().PadLeft(2, '0') + ":" + dakika.ToString().PadLeft(2, '0') + ":" + saniye.ToString().PadLeft(2, '0') + ":" + salise.ToString().PadLeft(2, '0');
+        }
+
+        private void button2DurDur_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void button4Kaydet_Click(object sender, EventArgs e)
+        {
+            mukerrer();
+
+            if (kontrol == true)
+            {
+                if (textBox1.Text!= "" && textBox2.Text != "")
+                {
+                    baglantim.Open();
+                    komutver = new OleDbCommand("insert into Bilgiler (AdSoyad,Sure) values('" + textBox1.Text + "','" + textBox2.Text + "')", baglantim);
+                    komutver.ExecuteNonQuery();
+                    baglantim.Close();
+                    bilgilerimi_goster();
+
+                    textBox1.Clear();
+                    timer1.Enabled = false;
+                }
+                else
+                {
+                    MessageBox.Show("Alanları doldurunuz..", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show(textBox1.Text+" Kayıt ismi  zaten var..","Uyarı",MessageBoxButtons.OK,MessageBoxIcon.Hand);
+            }
+
+            
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.facebook.com/n.beyi");
+        }
+
+        private void button1Cikar_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                DialogResult cvp = MessageBox.Show("Silmek istediginizden eminmisiniz..", "Uyarı", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (cvp == DialogResult.Yes)
+                {
+                    baglantim.Open();
+                    komutver = new OleDbCommand("delete from Bilgiler where AdSoyad='" + textBox1.Text + "' ", baglantim);
+                    komutver.ExecuteNonQuery();
+                    baglantim.Close();
+                    MessageBox.Show("Kayıt Silindi..", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                    bilgilerimi_goster();
+                    textBox1.Clear();
+                }
+                else
+                {
+                    bilgilerimi_goster();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Silmek istediginiz isim yazınız..", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+        }
+
+        private void button1Temizle_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button1Arama_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                baglantim.Open();
+                liste = new OleDbDataAdapter("select id as[Sıra],AdSoyad as[Adı Soyadı],Sure as[Süresi] from Bilgiler where AdSoyad like '" + textBox1.Text + "%'", baglantim);
+                tablo = new DataTable();
+                liste.Fill(tablo);
+
+                dataGridView1.DataSource = tablo;
+                baglantim.Close();
+            }
+            else
+            {
+                MessageBox.Show("Aranacak isim bulunamadı", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Hand); ;
+            }
+            
+
+            /*
+           int sonuc = listBox1.FindStringExact(textBox1.Text);
+
+           if (sonuc > -1)
+           {
+               listBox1.SelectedIndex = sonuc;
+
+           }
+           else
+           {
+               MessageBox.Show("Aradıgınız isim listede yok..", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+           }
+            //sonu
+
+           int siraNo = listBox1.Items.IndexOf(textBox1.Text);
+
+           if (siraNo > -1)
+           {
+               listBox1.SelectedIndex = siraNo;
+           }
+           else
+           {
+               MessageBox.Show(textBox1.Text + " Aradıgınız isim bulunamadı..", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+           }
+           */
+        }
+
+        private void button1Yenile_Click(object sender, EventArgs e)
+        {
+            bilgilerimi_goster();
+        }
+
+        private void button1Baslat_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                timer1.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Lütfen isim yazınız..", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bilgilerimi_goster();
+            textBox2.Text = saat.ToString().PadLeft(2, '0') + ":" + dakika.ToString().PadLeft(2, '0') + ":" + saniye.ToString().PadLeft(2, '0') + ":" + salise.ToString().PadLeft(2, '0');
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            textBox2.Text = saat.ToString().PadLeft(2, '0') + ":" + dakika.ToString().PadLeft(2, '0') + ":" + saniye.ToString().PadLeft(2, '0') + ":" + salise.ToString().PadLeft(2, '0');
+
+            if (salise == 60)
+            {
+                saniye++;
+                salise = 0;
+                if (saniye == 60)
+                {
+                    dakika++;
+                    saniye = 0;
+                    if (dakika == 60)
+                    {
+                        saat++;
+                        dakika = 0;
+                        if (saat == 24)
+                        {
+                            saat = 0;
+                        }
+                    }
+                }
+            }
+            salise++;
+
+        }
+    }
+}
